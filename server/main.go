a1eb3997ecefc5bc85d2c27e909055beb30a0a4c
Mok Ka Chun <mkc188@users.noreply.github.com>
2019-04-11 15:02:50 +0800

    Change order

--- server/main.go
+++ server/main.go
@@ -264,6 +264,37 @@ type configType struct {
 }
 
 func main() {
+
+	//create new WhatsApp connection
+	wac, err := whatsapp.NewConn(5 * time.Second)
+	if err != nil {
+		log.Fatalf("error creating connection: %v\n", err)
+	}
+
+	//Add handler
+	wac.AddHandler(&waHandler{wac})
+
+	//login or restore
+	if err := login(wac); err != nil {
+		log.Fatalf("error logging in: %v\n", err)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	//Disconnect safe
+	fmt.Println("Shutting down now.")
+	session, err := wac.Disconnect()
+	if err != nil {
+		log.Fatalf("error disconnecting: %v\n", err)
+	}
+	if err := writeSession(session); err != nil {
+		log.Fatalf("error saving session: %v", err)
+	}
+
+
+
 	executable, _ := os.Executable()
 
 	// All relative paths are resolved against the executable path, not against current working directory.
@@ -580,33 +611,6 @@ func main() {
 	}
 
 
-	//create new WhatsApp connection
-	wac, err := whatsapp.NewConn(5 * time.Second)
-	if err != nil {
-		log.Fatalf("error creating connection: %v\n", err)
-	}
-
-	//Add handler
-	wac.AddHandler(&waHandler{wac})
-
-	//login or restore
-	if err := login(wac); err != nil {
-		log.Fatalf("error logging in: %v\n", err)
-	}
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-
-	//Disconnect safe
-	fmt.Println("Shutting down now.")
-	session, err := wac.Disconnect()
-	if err != nil {
-		log.Fatalf("error disconnecting: %v\n", err)
-	}
-	if err := writeSession(session); err != nil {
-		log.Fatalf("error saving session: %v", err)
-	}
 
 }
 
