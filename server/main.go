f60648c87634507c2d65ee92a2db36fce1f4844f
Mok Ka Chun <mkc188@users.noreply.github.com>
2019-04-16 16:32:26 +0800

    Updated

--- server/main.go
+++ server/main.go
@@ -123,7 +123,7 @@ func (*waHandler) HandleTextMessage(message whatsapp.TextMessage) {
     client := pbx.NewNodeClient(globals.conn)
 
     // ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    ctx, cancel := context.WithTimeout(context.Background())
+    ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second)
     defer cancel()
 
     stream, err := client.MessageLoop(ctx)
