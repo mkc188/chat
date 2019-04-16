2a00085bb366aa1d22eea783d3d8186040fb5715
Mok Ka Chun <mkc188@users.noreply.github.com>
2019-04-16 15:37:16 +0800

    Updated

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 git add .
 git commit -a -m 'Updated'
 git push
-kill $(ps aux | grep '[/]home/mkc/go/.*tinode.conf' | awk '{print $2}')
-kill $(ps aux | grep '[/]home/mkc/go/.*tinode.conf' | awk '{print $2}')
+# kill $(ps aux | grep '[/]home/mkc/go/.*tinode.conf' | awk '{print $2}')
+# kill $(ps aux | grep '[/]home/mkc/go/.*tinode.conf' | awk '{print $2}')
 go get -tags rethinkdb github.com/mkc188/chat/server && go install -tags rethinkdb github.com/mkc188/chat/server
 $GOPATH/bin/server -config=$GOPATH/src/github.com/mkc188/chat/server/tinode.conf -static_data=$HOME/webapp
