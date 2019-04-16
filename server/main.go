2b904e8c6d926f7007a65679899bfd73dab09510
Mok Ka Chun <mkc188@users.noreply.github.com>
2019-04-16 16:58:00 +0800

    Updated

--- server/main.go
+++ server/main.go
@@ -108,7 +108,7 @@ func (*waHandler) HandleTextMessage(message whatsapp.TextMessage) {
   // log.Println("HandleTextMessage: session started", globals.sessionStore)
 
   var err error
-  var stream pbx.Node_MessageLoopClient
+  var stream *pbx.Node_MessageLoopClient
   if isConnEmpty {
     isConnEmpty = false
 
