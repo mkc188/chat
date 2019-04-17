187937cf074ac52e063f644280544a2715d515b3
Mok Ka Chun <mkc188@users.noreply.github.com>
2019-04-17 12:39:54 +0800

    Updated

--- server/main.go
+++ server/main.go
@@ -98,8 +98,8 @@ func (*waHandler) HandleTextMessage(message whatsapp.TextMessage) {
 
     sub := &pbx.ClientSub{}
     sub.Topic = "usrNoJ5tCr-JCM"
-    sub.SetDesc = 1
-    sub.SetSub = 2
+    sub.SetQuery.desc = 1
+    sub.SetQuery.sub = 2
     // sub.Content = []byte("hihi")
     msgSub := &pbx.ClientMsg_Sub{sub}
     clientMessage3 := &pbx.ClientMsg{Message: msgSub}
