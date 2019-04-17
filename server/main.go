89de3ddf320ba96e54261f84afdc7267ca68370d
Mok Ka Chun <mkc188@users.noreply.github.com>
2019-04-17 13:09:35 +0800

    Updated

--- server/main.go
+++ server/main.go
@@ -108,7 +108,7 @@ func (*waHandler) HandleTextMessage(message whatsapp.TextMessage) {
 
     note := &pbx.ClientNote{}
     note.Topic = "usrNoJ5tCr-JCM"
-    note.What = pbx.KP
+    note.What = 2
     msgNote := &pbx.ClientMsg_Note{note}
     clientMessage5 := &pbx.ClientMsg{Message: msgNote}
     err5 := globals.stream.Send(clientMessage5)
